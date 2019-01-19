4372e9c74051861eff291a2759d2fea10d5303d5
kenmylife <33402491+kenmylife@users.noreply.github.com>
2019-01-19 20:36:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,29 +2,7 @@ let auth = firebase.auth();
 let provider = new firebase.auth.FacebookAuthProvider(); //創造一個登入服務物件
 provider.addScope("user_birthday"); //要求的內容
 
-//觸動介面後，執行我們創的signIn函式，執行後會跑firebase提供的登入服務
-function signIn() {
-  firebase
-    .auth()
-    .signInWithPopup(provider)
-    .then(function(result) {
-      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-      var token = result.credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      // ...
-    })
-    .catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-    });
-}
+let currentUser = null; //儲存使用者資料
 
 //當使用者的登入狀態變化的時侯，執行我們的程式
 auth.onAuthStateChanged(function(user) {
@@ -33,12 +11,40 @@ auth.onAuthStateChanged(function(user) {
   member.style.display = "none";
   welcome.style.display = "none";
   if (user) {
-    // User is signed in.使用者登入
+    //使用者登入
+    currentUser = user; //給定使用者資料
     console.log(user);
     member.style.display = "block";
   } else {
-    // User is signed out. 使用者登出
+    //使用者登出
     console.log(user);
     welcome.style.display = "block";
   }
 });
+
+//觸動介面後，執行我們創的signIn函式，執行後會跑firebase提供的登入服務
+function signIn() {
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function(result) {})
+    .catch(function(error) {});
+}
+let db = firebase.database();
+
+function post() {
+  let id = currentUser.userid;
+  let name = currentUser.displayName;
+  let content = document.getElementById("content");
+  //加入資料庫
+  let ref = db.ref("./mageges");
+  ref.push({ id: id, name: name, content: content, time: Date.now() }, function(
+    error
+  ) {
+    if (error) {
+      alert(error);
+    } else {
+      alert("ok");
+    }
+  });
+}
