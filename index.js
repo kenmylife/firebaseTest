83e37f547b648bf668b5336b2d61d0a4beecaade
kenmylife <33402491+kenmylife@users.noreply.github.com>
2019-01-19 20:46:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function post() {
   let name = currentUser.displayName;
   let content = document.getElementById("content");
   //加入資料庫
-  let ref = db.ref("./message");
+  let ref = db.ref("/message");
   ref.push(
     { id: id, name: name, content: content, time: Date.now() }, //Date.now()另一種取得時間的方式
     function(error) {
